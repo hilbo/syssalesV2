d70fce9d77596adf106ba183405c79cfc5abf5eb
Hilbo Cardeira <hilbo.cardeira@gmail.com>
2022-08-04 14:04:48 -0300

    Created project

---
+++ src/main/java/com/system/syssalesv2/Syssalesv3Application.java
@@ -0,0 +1,13 @@
+package com.system.syssalesv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Syssalesv3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Syssalesv3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/syssalesv2/Syssalesv3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.syssalesv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Syssalesv3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
